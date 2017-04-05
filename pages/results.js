1da62d70855a49298e0b4f6301f08a1c4aadd902
Henry Tabima Giraldo <henrytabima.dev@gmail.com>
2017-04-05 11:18:06 -0500

    [add] fetch data for search

--- pages/results.js
+++ pages/results.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react'
+import fetch from 'isomorphic-fetch'
+import Router from 'next/router'
+import { ThemeProvider } from 'styled-components'
+import { searchTheme } from '../lib/themes'
+import Hero from '../components/Hero'
 
 class ResultsPage extends Component {
 
-  render() {
-    return (
 
+  static async getInitialProps({ query }) {
+    const URL = `https://api.spotify.com/v1/search/?q=${query.query}&type=artist,album,track`
+
+    const response = await fetch(URL)
+    const data = await response.json()
+
+    return data
+  }
+
+  render() {
+    console.log(this.props);
+    return (
+      <ThemeProvider theme={searchTheme}>
+        <div>
+        </div>
+      </ThemeProvider>
     )
   }
 
