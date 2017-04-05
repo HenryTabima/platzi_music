47637d62d4c79c8abad4370f91f5c1c2d96bc7de
Henry Tabima Giraldo <henrytabima.dev@gmail.com>
2017-04-05 18:54:18 -0500

    [add] serch Loading in page results

--- pages/results.js
+++ pages/results.js
@@ -2,27 +2,31 @@ import React, { Component } from 'react'
 import fetch from 'isomorphic-fetch'
 import Router from 'next/router'
 import { ThemeProvider } from 'styled-components'
+import { Grid } from 'react-styled-flexboxgrid'
 import { searchTheme } from '../lib/themes'
 import Hero from '../components/Hero'
+import Loading from '../components/Loading'
 
 class ResultsPage extends Component {
 
   state = {
-    searching: true
+    searching: false
   }
 
   static async getInitialProps({ query }) {
-    console.log(query);
-    const URL = `https://api.spotify.com/v1/search/?q=${query.query}&type=artist,album,track`
-
-    const response = await fetch(URL)
-    const data = await response.json()
-
-    return data
+    try {
+      const URL = `https://api.spotify.com/v1/search/?q=${query.query}&type=artist,album,track`
+      const response = await fetch(URL)
+      const data = await response.json()
+      console.log(data);
+      return data
+    } catch (error) {
+      throw error
+    }
   }
 
   handleSubmit = (event) => {
-    this.state({
+    this.setState({
       searching: true
     })
     event.preventDefault()
@@ -31,12 +35,24 @@ class ResultsPage extends Component {
     Router.push(`/results?query=${value}`)
   }
 
+  componentWillReceiveProps() {
+    this.setState({
+      searching: false
+    })
+  }
+
   render() {
-    console.log(this.props);
     return (
       <ThemeProvider theme={searchTheme}>
         <div>
           <Hero onSubmit={this.handleSubmit}/>
+          <Grid>
+            {
+              this.state.searching
+              ? <Loading/>
+              : <div>{this.props.artists.items[0].name}</div>
+            }
+          </Grid>
         </div>
       </ThemeProvider>
     )
