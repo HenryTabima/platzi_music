5d015051ee10a58a4e52cb0bba03462108b70086
Henry Tabima Giraldo <henrytabima.dev@gmail.com>
2017-04-05 08:18:23 -0500

    [add] main page with Hero, Loading and Footer

---
+++ pages/index.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import { Grid } from 'react-styled-flexboxgrid'
+import { ThemeProvider } from 'styled-components'
+import { indexTheme, searchTheme } from '../lib/themes.js'
+import '../lib/global'
+import Hero from '../components/Hero'
+import Footer from '../components/Footer'
+import Loading from '../components/Loading'
+
+class HomePage extends Component {
+
+  state = {
+    loading: false,
+  }
+
+  handleSubmit = (event) => {
+    this.setState({
+      loading: true
+    })
+    event.preventDefault()
+    const form = event.target
+    const value = form.elements.buscar.value
+    Router.push(`/results?query=${value}`)
+  }
+
+  render() {
+    return (
+      <ThemeProvider theme={this.state.loading ? searchTheme : indexTheme}>
+        <div>
+          <Hero/>
+          <Grid>
+            <Footer/>
+          </Grid>
+        </div>
+      </ThemeProvider>
+    )
+  }
+
+}
+
+export default HomePage
