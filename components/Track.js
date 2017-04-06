bca423cf4d45e51b8bb149faff951b4c9bbd20a2
Henry Tabima Giraldo <henrytabima.dev@gmail.com>
2017-04-06 11:10:30 -0500

    [add] tracks style

--- components/Track.js
+++ components/Track.js
@@ -1,10 +1,65 @@
-import React from 'react'
-import { Col } from 'react-styled-flexboxgrid'
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import { Row, Col } from 'react-styled-flexboxgrid'
+import { formattedTime } from '../lib/utils'
 
-function Track(props) {
-  return (
-    <Col xs={12}>{props.name}</Col>
-  )
+const TrackItem = styled(Col)`
+  margin-bottom: 1em;
+  color: ${props => props.theme.color.grayB};
+  border-bottom: 1px solid ${props => props.theme.color.grayA};
+  cursor: pointer;
+  transition: .3s;
+  padding: 0;
+  &:hover {
+    background: ${props => props.theme.color.grayB};
+    color: ${props => props.theme.color.grayA};
+  }
+`
+const Album = styled.div`
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+  color ${props=>props.theme.color.grayB};
+  span {
+    margin-left: 1em;
+    display: inline-block;
+  }
+`
+const Time = styled.div`
+  text-align: right;
+  padding-right: 1em;
+`
+
+class Track extends Component {
+  render () {
+    return (
+      <TrackItem xs={12}>
+        <Row middle='xs'>
+          <Col xs={12} sm={5}>
+            <Album>
+              <img
+                src={this.props.album.images[1].url}
+                width="70"
+                height="70"
+                alt={this.props.album.name}
+              />
+              <span>
+                {this.props.name}
+              </span>
+            </Album>
+          </Col>
+          <Col xs={8} sm={5}>
+            {this.props.album.name}
+          </Col>
+          <Col xs={4} sm={2}>
+            <Time>
+              {formattedTime(this.props.duration_ms)}
+            </Time>
+          </Col>
+        </Row>
+      </TrackItem>
+    )
+  }
 }
 
 export default Track
