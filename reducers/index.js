a70afc54e139e1ff1cd42029ea9baf067dbc988b
Henry Tabima Giraldo <henrytabima.dev@gmail.com>
2017-04-09 19:09:24 -0500

    [update] libs and reducers linter corrections

--- reducers/index.js
+++ reducers/index.js
@@ -1,11 +1,12 @@
-function reducer(state, action) {
-  switch(action.type) {
+// @flow
+function reducer(state: Object, action: Object) {
+  switch (action.type) {
     case 'SET_PLAYLIST':
       return Object.assign({}, state, { playlist: action.payload.playlist })
     case 'SET_CURRENT_TRACK':
-      return Object.assign({}, state, { currentTrack: action.payload.index})
+      return Object.assign({}, state, { currentTrack: action.payload.index })
     case 'SET_ALBUM_DATA':
-      return Object.assign({}, state, { album: action.payload.data})
+      return Object.assign({}, state, { album: action.payload.data })
     default:
       return state
   }
