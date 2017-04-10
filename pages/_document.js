b3b0fba0b098ad5142d61642458ed201ef64a4a3
Henry Tabima Giraldo <henrytabima.dev@gmail.com>
2017-04-09 22:49:07 -0500

    [update] disabel eslint danger in _document

--- pages/_document.js
+++ pages/_document.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-danger */
+
 import React from 'react'
 import Document, { Main, NextScript, Head } from 'next/document'
 import styleSheet from 'styled-components/lib/models/StyleSheet'
