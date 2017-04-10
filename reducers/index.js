dcdb8616bef5a134f31b91743ddc691b2236b444
Henry Tabima Giraldo <henrytabima.dev@gmail.com>
2017-04-10 16:31:55 -0500

    [update] return to not use immutable

--- reducers/index.js
+++ reducers/index.js
@@ -1,15 +1,11 @@
-// @flow
-// flow-disable-next-line
-import type { fromJS as Immut } from 'immutable'
-
-function reducer(state: Immut, action: { type: string, payload: any }) {
+function reducer(state, action) {
   switch (action.type) {
     case 'SET_PLAYLIST':
-      return state.set('playlist', action.payload.playlist)
+      return { ...state, playlist: action.payload.playlist }
     case 'SET_CURRENT_TRACK':
-      return state.set('currentTrack', action.payload.index)
+      return { ...state, currentTrack: action.payload.index }
     case 'SET_ALBUM_DATA':
-      return state.set('album', action.payload.data)
+      return { ...state, album: action.payload.data }
     default:
       return state
   }
